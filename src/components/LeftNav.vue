bae00ed157840cbbcc6cff53e75815f8bf1c8ca5
15915940450 <15915940450@139.com>
2018-04-16 21:10:08 +0800

    left nav 5

--- src/components/LeftNav.vue
+++ src/components/LeftNav.vue
@@ -1,14 +1,21 @@
 <template lang="html">
   <div class="left_nav">
-    <el-menu default-active="2" class="el-menu-vertical-menu" @open="handleOpen" @close="handleClose">
+    <el-menu default-active="1" class="el-menu-vertical-menu">
 
-      <el-menu-item index="2">
-        <i class="el-icon-menu"></i>
-        <span slot="title">导航二</span>
+      <el-menu-item index="1" class="nav_home">
+        <span slot="title">首页概览</span>
       </el-menu-item>
-      <el-menu-item index="4">
-        <i class="el-icon-setting"></i>
-        <span slot="title">导航四</span>
+      <el-menu-item index="2" class="nav_group">
+        <span slot="title">群组管理</span>
+      </el-menu-item>
+      <el-menu-item index="3" class="nav_money">
+        <span slot="title">押金管理</span>
+      </el-menu-item>
+      <el-menu-item index="4" class="nav_bike">
+        <span slot="title">车辆管理</span>
+      </el-menu-item>
+      <el-menu-item index="5" class="nav_system">
+        <span slot="title">系统管理</span>
       </el-menu-item>
 
     </el-menu>
@@ -19,13 +26,7 @@
 <script>
 export default {
   name:'LeftNav',
-  methods: {
-    handleOpen(key, keyPath) {
-      console.log(key, keyPath);
-    },
-    handleClose(key, keyPath) {
-      console.log(key, keyPath);
-    }
+  methods:{
   }
 };
 </script>
@@ -33,5 +34,42 @@ export default {
 <style lang="css" scoped>
 .left_nav .el-menu{
   height: calc(100vh - 62px);
+}
+
+.left_nav .nav_home{
+  background: url(../assets/nav_home.jpg) no-repeat 20px center;
+  padding-left: 50px !important;
+}
+.left_nav .nav_group{
+  background: url(../assets/nav_group.jpg) no-repeat 20px center;
+  padding-left: 50px !important;
+}
+.left_nav .nav_money{
+  background: url(../assets/nav_money.png) no-repeat 20px center;
+  padding-left: 50px !important;
+}
+.left_nav .nav_bike{
+  background: url(../assets/nav_bike.jpg) no-repeat 20px center;
+  padding-left: 50px !important;
+}
+.left_nav .nav_system{
+  background: url(../assets/nav_system.jpg) no-repeat 20px center;
+  padding-left: 50px !important;
+}
+/* active */
+.left_nav .nav_home.is-active{
+  background: url(../assets/nav_home_active.jpg) no-repeat 20px center;
+}
+.left_nav .nav_group.is-active{
+  background: url(../assets/nav_group_active.jpg) no-repeat 20px center;
+}
+.left_nav .nav_money.is-active{
+  background: url(../assets/nav_money_active.jpg) no-repeat 20px center;
+}
+.left_nav .nav_bike.is-active{
+  background: url(../assets/nav_bike_active.jpg) no-repeat 20px center;
+}
+.left_nav .nav_system.is-active{
+  background: url(../assets/nav_system_active.png) no-repeat 20px center;
 }
 </style>
