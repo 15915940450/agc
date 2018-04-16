fcd15d6c77b3976611d31e77f241a6485fc61987
15915940450 <15915940450@139.com>
2018-04-16 13:55:55 +0800

    component joy

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import HelloWorld from '@/components/HelloWorld';
+import CJoy from '@/components/CJoy';
 
 Vue.use(Router);
 
@@ -8,8 +8,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'CJoy',
+      component: CJoy
     }
   ]
 });
