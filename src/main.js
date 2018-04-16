a830241db9b7745026007c4e6735fda72ecf09a9
15915940450 <15915940450@139.com>
2018-04-16 13:46:41 +0800

    use EUI

--- src/main.js
+++ src/main.js
@@ -1,11 +1,16 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
-import App from './App';
 import router from './router';
 
 import im from 'axios';
 import apiURLs from './api';
+import EUI from 'element-ui';
+import './assets/e_theme.scss';
+
+import App from './App';
+
+Vue.use(EUI);
 
 //axios=>$im
 Vue.prototype.$im=im;
