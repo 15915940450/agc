50f9a9d6bbb3a8e03567d58c09a3c22220b8e00b
15915940450 <15915940450@139.com>
2018-04-17 17:51:11 +0800

    kefu

--- src/components/NeedLogin.vue
+++ src/components/NeedLogin.vue
@@ -11,12 +11,20 @@
         <el-form-item>
           <el-input type="password" v-model="form.password" auto-complete="off" placeholder="请输入密码" class="need_login-password"></el-input>
         </el-form-item>
+        <el-form-item class="need_login-vcode">
+          <el-input v-model="form.validateCode" auto-complete="off" placeholder="请输入验证码"></el-input>
+          <img src="../assets/validateCode.jpeg" height="30" />
+        </el-form-item>
+        <el-form-item>
+          <p class="need_login-hint">* 手机号码不正确</p>
+        </el-form-item>
       </el-form>
       <div slot="footer" class="dialog-footer">
         <el-button type="success" @click="dialogFormVisible = false" id="btn_login">登录</el-button>
       </div>
     </el-dialog>
 
+    <p class="kefu">客服：400-618-7238     2018 ◎深圳易马达科技有限公司版权所有</p>
   </div>
 </template>
 
@@ -25,7 +33,7 @@ export default {
   name:'NeedLogin',
   data() {
     return {
-      dialogFormVisible: false,
+      dialogFormVisible: true,
       width:'380px',
       customClass:'onelogin',
       form: {
@@ -43,7 +51,7 @@ export default {
 }
 #btn_login{
   width:100%;
-  margin-bottom: 70px;
+  margin-bottom: 40px;
 }
 .project_name{
   font-size: 26px;
@@ -51,6 +59,28 @@ export default {
   margin-bottom: 30px;
   margin-top: 30px;
   color: #333;
+  text-align: center;
+}
+.need_login-vcode{
+  position: relative;
+}
+.need_login-vcode img{
+  position: absolute;
+  right:5px;
+  top:5px;
+}
+.need_login-hint{
+  color:red;
+  font-size: 12px;
+}
+.kefu{
+  position: fixed;
+  bottom: 20px;
+  left: 0;
+  color:#FFF;
+  font-size: 16px;
+  z-index: 10000;
+  width: 100%;
   text-align: center;
 }
 </style>
