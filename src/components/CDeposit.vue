e178d3a76152690ad80b58b249f73d0994b5ca55
15915940450 <15915940450@139.com>
2018-04-17 16:17:10 +0800

    form top up

--- src/components/CDeposit.vue
+++ src/components/CDeposit.vue
@@ -14,7 +14,16 @@
             <el-col :span="14">
               <h3 class="im_card-title">
                 我的押金(元)
-                <a href="javascript:;" class="im_card-top_up">充值</a>
+                <a href="javascript:;" v-popover:top_up class="im_card-top_up">充值</a>
+                <el-popover
+                  ref="top_up"
+                  placement="bottom"
+                  width="400"
+                  trigger="click">
+                  <!-- 表单 -->
+                  <FormTopUp />
+
+                </el-popover>
               </h3>
               <p class="im_card-value">
                 <icon name="eye-slash" scale="1.3"></icon>
@@ -46,7 +55,6 @@
           </el-row>
         </div>
       </el-col>
-
       <!-- 可用电池数(个) -->
       <el-col :span="6">
         <div class="im_card">
@@ -146,6 +154,8 @@
 </template>
 
 <script>
+import FormTopUp from './FormTopUp.vue';
+
 export default {
   name:'CDeposit',
   data() {
@@ -159,56 +169,14 @@ export default {
         address: '上海市普陀区真北路',
         shop: '王小虎夫妻店',
         shopId: '10333'
-      }, {
-        id: '12987123',
-        name: '好滋好味鸡蛋仔',
-        category: '江浙小吃、小吃零食',
-        desc: '荷兰优质淡奶，奶香浓而不腻',
-        address: '上海市普陀区真北路',
-        shop: '王小虎夫妻店',
-        shopId: '10333'
-      }, {
-        id: '12987125',
-        name: '好滋好味鸡蛋仔',
-        category: '江浙小吃、小吃零食',
-        desc: '荷兰优质淡奶，奶香浓而不腻',
-        address: '上海市普陀区真北路',
-        shop: '王小虎夫妻店',
-        shopId: '10333'
-      }, {
-        id: '12987126',
-        name: '好滋好味鸡蛋仔',
-        category: '江浙小吃、小吃零食',
-        desc: '荷兰优质淡奶，奶香浓而不腻',
-        address: '上海市普陀区真北路',
-        shop: '王小虎夫妻店',
-        shopId: '10333'
-      }, {
-        id: '12987126',
-        name: '好滋好味鸡蛋仔',
-        category: '江浙小吃、小吃零食',
-        desc: '荷兰优质淡奶，奶香浓而不腻',
-        address: '上海市普陀区真北路',
-        shop: '王小虎夫妻店',
-        shopId: '10333'
-      }, {
-        id: '12987126',
-        name: '好滋好味鸡蛋仔',
-        category: '江浙小吃、小吃零食',
-        desc: '荷兰优质淡奶，奶香浓而不腻',
-        address: '上海市普陀区真北路',
-        shop: '王小虎夫妻店',
-        shopId: '10333'
-      }, {
-        id: '12987126',
-        name: '好滋好味鸡蛋仔',
-        category: '江浙小吃、小吃零食',
-        desc: '荷兰优质淡奶，奶香浓而不腻',
-        address: '上海市普陀区真北路',
-        shop: '王小虎夫妻店',
-        shopId: '10333'
       }]
     };
+  },
+  components:{
+    FormTopUp
+  },
+  methods: {
+
   }
 };
 </script>
---
+++ src/components/FormTopUp.vue
@@ -0,0 +1,58 @@
+<template lang="html">
+  <el-form ref="form" :model="dataTopUp" label-width="90px" size="mini">
+    <el-form-item>
+      <h4>请输入数量，每个虚拟电池退还押金 <span>999.00</span>元</h4>
+    </el-form-item>
+
+    <!-- 数量 -->
+    <el-form-item label="数量（个）">
+      <el-input v-model="dataTopUp.name"></el-input>
+    </el-form-item>
+
+    <el-form-item label="金额(元)">
+      {{dataTopUp.amount}}
+    </el-form-item>
+    <el-form-item label="支付方式">
+      <el-radio-group v-model="dataTopUp.payType" size="">
+        <el-radio border label="1">支付宝</el-radio>
+        <el-radio border label="2">微信</el-radio>
+      </el-radio-group>
+    </el-form-item>
+
+
+    <el-form-item size="">
+      <el-button>取消</el-button>
+      <el-button type="primary" @click="onSubmit">确定</el-button>
+    </el-form-item>
+  </el-form>
+</template>
+
+<script>
+export default {
+  name:'FormTopUp',
+  data:function(){
+    return ({
+      dataTopUp: {
+        name: '',
+        amount:9099,
+        region: '',
+        date1: '',
+        date2: '',
+        delivery: false,
+        type: [],
+        payType: '2',
+        desc: ''
+      }
+
+    });
+  },
+  methods:{
+    onSubmit() {
+      console.log('submit!');
+    }
+  }
+};
+</script>
+
+<style lang="css" scoped>
+</style>
