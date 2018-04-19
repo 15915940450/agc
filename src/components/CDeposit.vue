d91836a994da497dee6316e9c8a481b32d49e49d
15915940450 <15915940450@139.com>
2018-04-19 10:04:54 +0800

    empty deposit

--- src/components/CDeposit.vue
+++ src/components/CDeposit.vue
@@ -1,156 +1,164 @@
 <template lang="html">
   <div class="component_deposit">
-    <el-row :gutter="10">
+    <div v-if="deposit">
+      <el-row :gutter="10">
 
-      <!-- 押金 -->
-      <el-col :span="6">
-        <div class="im_card">
-          <el-row>
-            <el-col :span="10">
-              <div class="im_card-icon">
-                押金
-              </div>
-            </el-col>
-            <el-col :span="14">
-              <h3 class="im_card-title">
-                我的押金(元)
-                <a href="javascript:;" v-popover:top_up class="im_card-top_up">充值</a>
-                <el-popover
-                  ref="top_up"
-                  placement="bottom"
-                  width="400"
-                  trigger="click">
-                  <!-- 表单 -->
-                  <FormTopUp />
+        <!-- 押金 -->
+        <el-col :span="6">
+          <div class="im_card">
+            <el-row>
+              <el-col :span="10">
+                <div class="im_card-icon">
+                  押金
+                </div>
+              </el-col>
+              <el-col :span="14">
+                <h3 class="im_card-title">
+                  我的押金(元)
+                  <a href="javascript:;" v-popover:top_up class="im_card-top_up">充值</a>
+                  <el-popover
+                    ref="top_up"
+                    placement="bottom"
+                    width="400"
+                    trigger="click">
+                    <!-- 表单 -->
+                    <FormTopUp />
 
-                </el-popover>
-              </h3>
-              <p class="im_card-value">
-                <icon name="eye-slash" scale="1.3"></icon>
-                7988
-              </p>
-            </el-col>
-          </el-row>
-        </div>
-      </el-col>
-      <!-- 可退押金 -->
-      <el-col :span="6">
-        <div class="im_card">
-          <el-row>
-            <el-col :span="10">
-              <div class="im_card-icon">
-                可退押金
-              </div>
-            </el-col>
-            <el-col :span="14">
-              <h3 class="im_card-title">
-                可退押金(元)
-                <a href="javascript:;" class="im_card-top_up">退款</a>
-              </h3>
-              <p class="im_card-value">
-                <icon name="eye-slash" scale="1.3"></icon>
-                90
-              </p>
-            </el-col>
-          </el-row>
-        </div>
-      </el-col>
-      <!-- 可用电池数(个) -->
-      <el-col :span="6">
-        <div class="im_card">
-          <el-row>
-            <el-col :span="10">
-              <div class="im_card-icon">
-                可用电池数
-              </div>
-            </el-col>
-            <el-col :span="14">
-              <h3 class="im_card-title">
-                可用电池数(个)
-              </h3>
-              <p class="im_card-value">
-                22988
-              </p>
-            </el-col>
-          </el-row>
-        </div>
-      </el-col>
-      <!-- 已用电池数(个) -->
-      <el-col :span="6">
-        <div class="im_card">
-          <el-row>
-            <el-col :span="10">
-              <div class="im_card-icon">
-              已用电池数
-              </div>
-            </el-col>
-            <el-col :span="14">
-              <h3 class="im_card-title">
-                已用电池数(个)
-              </h3>
-              <p class="im_card-value">
-                3000
-              </p>
-            </el-col>
-          </el-row>
-        </div>
-      </el-col>
+                  </el-popover>
+                </h3>
+                <p class="im_card-value">
+                  <icon name="eye-slash" scale="1.3"></icon>
+                  7988
+                </p>
+              </el-col>
+            </el-row>
+          </div>
+        </el-col>
+        <!-- 可退押金 -->
+        <el-col :span="6">
+          <div class="im_card">
+            <el-row>
+              <el-col :span="10">
+                <div class="im_card-icon">
+                  可退押金
+                </div>
+              </el-col>
+              <el-col :span="14">
+                <h3 class="im_card-title">
+                  可退押金(元)
+                  <a href="javascript:;" class="im_card-top_up">退款</a>
+                </h3>
+                <p class="im_card-value">
+                  <icon name="eye-slash" scale="1.3"></icon>
+                  90
+                </p>
+              </el-col>
+            </el-row>
+          </div>
+        </el-col>
+        <!-- 可用电池数(个) -->
+        <el-col :span="6">
+          <div class="im_card">
+            <el-row>
+              <el-col :span="10">
+                <div class="im_card-icon">
+                  可用电池数
+                </div>
+              </el-col>
+              <el-col :span="14">
+                <h3 class="im_card-title">
+                  可用电池数(个)
+                </h3>
+                <p class="im_card-value">
+                  22988
+                </p>
+              </el-col>
+            </el-row>
+          </div>
+        </el-col>
+        <!-- 已用电池数(个) -->
+        <el-col :span="6">
+          <div class="im_card">
+            <el-row>
+              <el-col :span="10">
+                <div class="im_card-icon">
+                已用电池数
+                </div>
+              </el-col>
+              <el-col :span="14">
+                <h3 class="im_card-title">
+                  已用电池数(个)
+                </h3>
+                <p class="im_card-value">
+                  3000
+                </p>
+              </el-col>
+            </el-row>
+          </div>
+        </el-col>
 
-    </el-row>
+      </el-row>
 
-    <div class="table_wrap">
-      <h3 class="title">押金记录</h3>
-      <!-- 押金记录表格 -->
-      <el-table
-        :data="tableData5" stripe
-        style="width: 100%" class="table_wrap-table">
-        <!-- 展开行 -->
-        <el-table-column type="expand">
-          <template slot-scope="props">
-            <el-form label-position="left" inline class="im-table-expand">
-              <el-form-item label="商品名称">
-                <span>{{ props.row.name }}</span>
-              </el-form-item>
-              <el-form-item label="所属店铺">
-                <span>{{ props.row.shop }}</span>
-              </el-form-item>
-              <el-form-item label="商品 ID">
-                <span>{{ props.row.id }}</span>
-              </el-form-item>
-              <el-form-item label="店铺 ID">
-                <span>{{ props.row.shopId }}</span>
-              </el-form-item>
-              <el-form-item label="商品分类">
-                <span>{{ props.row.category }}</span>
-              </el-form-item>
-              <el-form-item label="店铺地址">
-                <span>{{ props.row.address }}</span>
-              </el-form-item>
-              <el-form-item label="商品描述">
-                <span>{{ props.row.desc }}</span>
-              </el-form-item>
-            </el-form>
-          </template>
-        </el-table-column>
-        <el-table-column
-          label="商品 ID"
-          prop="id">
-        </el-table-column>
-        <el-table-column
-          label="商品名称"
-          prop="name">
-        </el-table-column>
-        <el-table-column
-          label="描述"
-          prop="desc">
-        </el-table-column>
-      </el-table>
-      <!-- 分页 -->
-      <el-pagination :background="true" layout="total,->,jumper,prev,pager,next" :total="93" class="table_wrap-pagination">
+      <div class="table_wrap">
+        <h3 class="title">押金记录</h3>
+        <!-- 押金记录表格 -->
+        <el-table
+          :data="tableData5" stripe
+          style="width: 100%" class="table_wrap-table">
+          <!-- 展开行 -->
+          <el-table-column type="expand">
+            <template slot-scope="props">
+              <el-form label-position="left" inline class="im-table-expand">
+                <el-form-item label="商品名称">
+                  <span>{{ props.row.name }}</span>
+                </el-form-item>
+                <el-form-item label="所属店铺">
+                  <span>{{ props.row.shop }}</span>
+                </el-form-item>
+                <el-form-item label="商品 ID">
+                  <span>{{ props.row.id }}</span>
+                </el-form-item>
+                <el-form-item label="店铺 ID">
+                  <span>{{ props.row.shopId }}</span>
+                </el-form-item>
+                <el-form-item label="商品分类">
+                  <span>{{ props.row.category }}</span>
+                </el-form-item>
+                <el-form-item label="店铺地址">
+                  <span>{{ props.row.address }}</span>
+                </el-form-item>
+                <el-form-item label="商品描述">
+                  <span>{{ props.row.desc }}</span>
+                </el-form-item>
+              </el-form>
+            </template>
+          </el-table-column>
+          <el-table-column
+            label="商品 ID"
+            prop="id">
+          </el-table-column>
+          <el-table-column
+            label="商品名称"
+            prop="name">
+          </el-table-column>
+          <el-table-column
+            label="描述"
+            prop="desc">
+          </el-table-column>
+        </el-table>
+        <!-- 分页 -->
+        <el-pagination :background="true" layout="total,->,jumper,prev,pager,next" :total="93" class="table_wrap-pagination">
 
-      </el-pagination>
+        </el-pagination>
+      </div>
+
     </div>
-
+    <div v-if="!deposit" class="empty_deposit">
+      <h3 class="title">我的押金</h3>
+      <img src="../assets/empty_deposit.png" />
+      <p>您还没有押金哦！</p>
+      <el-button type="primary" size="small">充值</el-button>
+    </div>
   </div>
 </template>
 
@@ -170,7 +178,8 @@ export default {
         address: '上海市普陀区真北路',
         shop: '王小虎夫妻店',
         shopId: '10333'
-      }]
+      }],
+      deposit:0
     };
   },
   components:{
@@ -245,5 +254,22 @@ export default {
   padding-top:20px;
   padding-bottom:20px;
 }
-
+/* 押金为空 */
+.empty_deposit{
+  background: #FFF;
+  text-align: center;
+  padding-bottom: 30px;
+  min-height: calc(100vh - 60px - 60px - 30px);
+}
+.empty_deposit .title{
+  text-align: left;
+}
+.empty_deposit img{
+  margin-top:100px;
+}
+.empty_deposit p{
+  color: #999;
+  font-size: 14px;
+  margin-bottom: 30px;
+}
 </style>
