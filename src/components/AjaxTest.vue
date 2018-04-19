50cb208f5794de2849ac295ea032d56f3ed10850
15915940450 <15915940450@139.com>
2018-04-19 16:06:46 +0800

    8764,8762

--- src/components/AjaxTest.vue
+++ src/components/AjaxTest.vue
@@ -5,8 +5,29 @@
 </template>
 
 <script>
+var HOST='';
+// var xmlhttp;
+// var url='';
+// if (window.XMLHttpRequest) {// code for IE7+, Firefox, Chrome, Opera, Safari
+//   xmlhttp=new XMLHttpRequest();
+// } else {// code for IE6, IE5
+//   xmlhttp=new ActiveXObject('Microsoft.XMLHTTP');
+// }
+// xmlhttp.onreadystatechange=function() {
+//   if (xmlhttp.readyState===4 && xmlhttp.status===200) {
+//     // console.log(xmlhttp.responseText);
+//   }
+// };
+// xmlhttp.open('GET',url,true);
+// xmlhttp.send();
+
+//1.POST /account/baseInfo
+
 var xmlhttp;
-var url='';
+var url=HOST+'/account/baseInfo';
+var sendData={
+  phone:'15820480937'
+};
 if (window.XMLHttpRequest) {// code for IE7+, Firefox, Chrome, Opera, Safari
   xmlhttp=new XMLHttpRequest();
 } else {// code for IE6, IE5
@@ -14,15 +35,17 @@ if (window.XMLHttpRequest) {// code for IE7+, Firefox, Chrome, Opera, Safari
 }
 xmlhttp.onreadystatechange=function() {
   if (xmlhttp.readyState===4 && xmlhttp.status===200) {
-    // console.log(xmlhttp.responseText);
+    console.log(xmlhttp.responseText);
   }
 };
-xmlhttp.open('GET',url,true);
-xmlhttp.send();
+xmlhttp.open('POST',url,true);
+xmlhttp.setRequestHeader('Content-type','application/x-www-form-urlencoded');
+xmlhttp.send(JSON.stringify(sendData));
 
 export default {
   name:'AjaxTest'
 };
+
 </script>
 
 <style lang="css" scoped>
--- src/components/CDeposit.vue
+++ src/components/CDeposit.vue
@@ -179,7 +179,7 @@ export default {
         shop: '王小虎夫妻店',
         shopId: '10333'
       }],
-      deposit:0
+      deposit:9
     };
   },
   components:{
