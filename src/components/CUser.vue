e3e14e4da931c336d83462e7a17808a112f3460d
15915940450 <15915940450@139.com>
2018-04-19 21:34:48 +0800

    empty user

--- src/components/CUser.vue
+++ src/components/CUser.vue
@@ -8,9 +8,14 @@
       </el-breadcrumb>
     </div>
 
-    <div class="user_list">
+    <div class="user_list" v-if="user">
       <!-- table wrap -->
       <BaseTableWrap />
+    </div>
+    <div v-if="!user" class="empty_user">
+      <img src="../assets/empty_user.png" />
+      <p>您还没有用户哦！</p>
+      <el-button type="primary" size="small">新建</el-button>
     </div>
 
     <!-- 新建 -->
@@ -25,6 +30,11 @@ import BaseModalWrap from './BaseModalWrap';
 
 export default {
   name:'CUser',
+  data:function(){
+    return ({
+      user:0
+    });
+  },
   components:{
     BaseTableWrap,
     BaseModalWrap
@@ -43,5 +53,24 @@ export default {
 .user_list{
   background: #FFF;
   margin-top: 20px;
+}
+/* 没有用户 */
+.empty_user{
+  background: #FFF;
+  text-align: center;
+  padding-bottom: 30px;
+  margin-top:20px;
+  min-height: calc(100vh - 60px - 60px - 20px - 40px - 30px);
+}
+.empty_user .title{
+  text-align: left;
+}
+.empty_user img{
+  margin-top:100px;
+}
+.empty_user p{
+  color: #999;
+  font-size: 14px;
+  margin-bottom: 30px;
 }
 </style>
