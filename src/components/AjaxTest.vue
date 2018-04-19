b2544ca9ecaacaaa5600b7fef94090e4fbe2b8de
15915940450 <15915940450@139.com>
2018-04-19 16:29:48 +0800

    urls

--- src/components/AjaxTest.vue
+++ src/components/AjaxTest.vue
@@ -5,7 +5,7 @@
 </template>
 
 <script>
-var HOST='';
+// var HOST='http://119.23.133.72:8764/agent';
 // var xmlhttp;
 // var url='';
 // if (window.XMLHttpRequest) {// code for IE7+, Firefox, Chrome, Opera, Safari
@@ -22,25 +22,43 @@ var HOST='';
 // xmlhttp.send();
 
 //1.POST /account/baseInfo
+// var url=HOST+'/account/baseInfo';
+// var sendData={
+//   phone:'15820480937'
+// };
+// function imPostForm(url,sendData,callback){
+//   var callback=callback || function(){};
+//   var xmlhttp;
+//
+//
+//   if (window.XMLHttpRequest) {// code for IE7+, Firefox, Chrome, Opera, Safari
+//     xmlhttp=new XMLHttpRequest();
+//   } else {// code for IE6, IE5
+//     xmlhttp=new ActiveXObject('Microsoft.XMLHTTP');
+//   }
+//   xmlhttp.onreadystatechange=function() {
+//     if (xmlhttp.readyState===4 && xmlhttp.status===200) {
+//       // console.log(xmlhttp.responseText);
+//       callback(xmlhttp.responseText);
+//     }
+//   };
+//   xmlhttp.open('POST',url,true);
+//   xmlhttp.setRequestHeader('Content-type','application/x-www-form-urlencoded');
+//   xmlhttp.send(JSON.stringify(sendData));
+// }
+// imPostForm(url,sendData,function(rps){
+//   console.log(rps);
+// });
+import {urls,imPostForm} from '../api/urls.js';
+console.log(urls);
+console.log(imPostForm);
 
-var xmlhttp;
-var url=HOST+'/account/baseInfo';
 var sendData={
   phone:'15820480937'
 };
-if (window.XMLHttpRequest) {// code for IE7+, Firefox, Chrome, Opera, Safari
-  xmlhttp=new XMLHttpRequest();
-} else {// code for IE6, IE5
-  xmlhttp=new ActiveXObject('Microsoft.XMLHTTP');
-}
-xmlhttp.onreadystatechange=function() {
-  if (xmlhttp.readyState===4 && xmlhttp.status===200) {
-    console.log(xmlhttp.responseText);
-  }
-};
-xmlhttp.open('POST',url,true);
-xmlhttp.setRequestHeader('Content-type','application/x-www-form-urlencoded');
-xmlhttp.send(JSON.stringify(sendData));
+imPostForm(urls.baseInfo,sendData,function(rps){
+  console.log(rps);
+});
 
 export default {
   name:'AjaxTest'
