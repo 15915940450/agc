45e78419c0868aae43e4b492f3fa9636ec8931f4
15915940450 <15915940450@139.com>
2018-04-21 16:06:42 +0800

    nav active in store

--- src/components/LeftNav.vue
+++ src/components/LeftNav.vue
@@ -1,24 +1,11 @@
 <template lang="html">
   <div class="left_nav">
-    <el-menu :default-active="defaultNavActive" class="el-menu-vertical-menu">
-
-      <el-menu-item index="1" class="nav_home" @click="rrPush('/')">
-        <span slot="title">首页概览</span>
-      </el-menu-item>
-      <el-menu-item index="2" class="nav_group" @click="rrPush('/group')">
-        <span slot="title">群组管理</span>
-      </el-menu-item>
-      <el-menu-item index="3" class="nav_money" @click="rrPush('/deposit/1')">
-        <span slot="title">押金管理</span>
-      </el-menu-item>
-      <el-menu-item index="4" class="nav_bike" @click="rrPush('/evs/1')">
-        <span slot="title">车辆管理</span>
-      </el-menu-item>
-      <el-menu-item index="5" class="nav_system" @click="rrPush('/sys')">
-        <span slot="title">系统管理</span>
-      </el-menu-item>
-
-    </el-menu>
+    <ul id="im_nav">
+      <li v-for="(item) in imNav" :class="{active:isActive===item.id}" :key="item.id">
+        <!-- <router-link :to="item.link" @click="rrPush(item)">{{item.name}}</router-link>{{navActive.navActive}} -->
+        <a href="javascript:;" @click="rrPush(item)">{{item.name}}</a>
+      </li>
+    </ul>
 
   </div>
 </template>
@@ -31,73 +18,144 @@ export default {
   name:'LeftNav',
   data:function(){
     return ({
-      defaultNavActive:this.findDefaultNavActive()
+      imNav:[
+        {
+          id:1,
+          name:'首页概览',
+          link:'/'
+        },
+        {
+          id:2,
+          name:'群组管理',
+          link:'/group'
+        },
+        {
+          id:3,
+          name:'押金管理',
+          link:'/deposit/1'
+        },
+        {
+          id:4,
+          name:'车辆管理',
+          link:'/evs/1'
+        },
+        {
+          id:5,
+          name:'系统管理',
+          link:'/sys'
+        }
+      ]
     });
   },
   computed:{
-    ...mapState(['navActive'])
+    ...mapState(['navActive']),
+    isActive:function(){
+      return (this.navActive.navActive);
+    }
   },
   methods:{
-    rrPush:function(rr){
+    rrPush:function(item){
+      // 点击导航的时候 设置当前导航active
+      this.$store.commit('setNavActive',item.id);
+      // 编程式导航 router.push({ path: `/user/${userId}` }) // -> /user/123
       this.$router.push({
-        path:rr
+        path:item.link
       });
-      // router.push({ path: `/user/${userId}` }) // -> /user/123
+
     },
     findDefaultNavActive:function(){
+      var vueThis=this;
       var arrRs=['pre_http://localhost:1590/#/_fix','group','deposit','evs','sys'];
       var defaultNavActive='1';
       arrRs.forEach(function(v,i){
         if(window.location.href.indexOf(v)!==-1){
-          defaultNavActive=''+(i+1);
-          // console.log(defaultNavActive);
+          defaultNavActive=(i+1);
+          vueThis.$store.commit('setNavActive',defaultNavActive);
         }
       });
       return defaultNavActive;
     }
+  },
+  created:function(){
+    //1.创建的时候找出当前激活的导航
+    this.findDefaultNavActive();
   }
 };
 </script>
 
 <style lang="css" scoped>
-.left_nav .el-menu{
-  height: calc(100vh - 62px);
-}
+  #im_nav,#im_nav *{
+    margin: 0;
+    padding: 0;
+  }
+  #im_nav{}
+  #im_nav li{
+    height: 50px;
+    /* border-bottom: 1px solid #90DEFF; */
+  }
 
-.left_nav .nav_home{
-  background: url(../assets/nav_home.jpg) no-repeat 20px center;
-  padding-left: 50px !important;
-}
-.left_nav .nav_group{
-  background: url(../assets/nav_group.jpg) no-repeat 20px center;
-  padding-left: 50px !important;
-}
-.left_nav .nav_money{
-  background: url(../assets/nav_money.png) no-repeat 20px center;
-  padding-left: 50px !important;
-}
-.left_nav .nav_bike{
-  background: url(../assets/nav_bike.jpg) no-repeat 20px center;
-  padding-left: 50px !important;
-}
-.left_nav .nav_system{
-  background: url(../assets/nav_system.jpg) no-repeat 20px center;
-  padding-left: 50px !important;
-}
-/* active */
-.left_nav .nav_home.is-active{
-  background: url(../assets/nav_home_active.jpg) no-repeat 20px center;
-}
-.left_nav .nav_group.is-active{
-  background: url(../assets/nav_group_active.jpg) no-repeat 20px center;
-}
-.left_nav .nav_money.is-active{
-  background: url(../assets/nav_money_active.jpg) no-repeat 20px center;
-}
-.left_nav .nav_bike.is-active{
-  background: url(../assets/nav_bike_active.jpg) no-repeat 20px center;
-}
-.left_nav .nav_system.is-active{
-  background: url(../assets/nav_system_active.png) no-repeat 20px center;
-}
+  #im_nav li a{
+    display: block;
+    font-size: 14px;
+    color:#333333;
+    line-height: 50px;
+    padding-left: 50px;
+    border-left:3px solid #FFF;
+  }
+  #im_nav li:hover a{
+    border-left:3px solid #FFF;
+    color:#FF6600;
+  }
+  #im_nav li.active a{
+    border-left:3px solid #FF6600;
+    color:#FF6600;
+  }
+  #im_nav li:nth-child(1) a{
+    background: url(../assets/nav_home.jpg) no-repeat 20px center;
+  }
+  #im_nav li:nth-child(2) a{
+    background: url(../assets/nav_group.jpg) no-repeat 20px center;
+  }
+  #im_nav li:nth-child(3) a{
+    background: url(../assets/nav_money.png) no-repeat 20px center;
+  }
+  #im_nav li:nth-child(4) a{
+    background: url(../assets/nav_bike.jpg) no-repeat 20px center;
+  }
+  #im_nav li:nth-child(5) a{
+    background: url(../assets/nav_system.jpg) no-repeat 20px center;
+  }
+
+  #im_nav li:nth-child(1):hover a{
+    background: url(../assets/nav_home_active.jpg) no-repeat 20px center;
+  }
+  #im_nav li:nth-child(2):hover a{
+    background: url(../assets/nav_group_active.jpg) no-repeat 20px center;
+  }
+  #im_nav li:nth-child(3):hover a{
+    background: url(../assets/nav_money_active.jpg) no-repeat 20px center;
+  }
+  #im_nav li:nth-child(4):hover a{
+    background: url(../assets/nav_bike_active.jpg) no-repeat 20px center;
+  }
+  #im_nav li:nth-child(5):hover a{
+    background: url(../assets/nav_system_active.png) no-repeat 20px center;
+  }
+  #im_nav li:nth-child(1).active a{
+    background: url(../assets/nav_home_active.jpg) no-repeat 20px center;
+  }
+  #im_nav li:nth-child(2).active a{
+    background: url(../assets/nav_group_active.jpg) no-repeat 20px center;
+  }
+  #im_nav li:nth-child(3).active a{
+    background: url(../assets/nav_money_active.jpg) no-repeat 20px center;
+  }
+  #im_nav li:nth-child(4).active a{
+    background: url(../assets/nav_bike_active.jpg) no-repeat 20px center;
+  }
+  #im_nav li:nth-child(5).active a{
+    background: url(../assets/nav_system_active.png) no-repeat 20px center;
+  }
+
+
 </style>
--- src/store/modules/navActive.js
+++ src/store/modules/navActive.js
@@ -1,5 +1,5 @@
 var state={
-  navActive:'1' //1,2,3,4,5...
+  navActive:1 //1,2,3,4,5...
 };
 
 var mutations={
