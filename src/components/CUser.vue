14d2f403869ee02775f6b4287649844bdc2c4558
15915940450 <15915940450@139.com>
2018-04-25 17:36:40 +0800

    new user

--- src/components/CUser.vue
+++ src/components/CUser.vue
@@ -88,7 +88,35 @@
     </div>
 
     <!-- 新建 -->
-    <BaseModalWrap />
+    <div class="modal_wrap">
+      <el-dialog
+        title="添加用户"
+        :visible.sync="modalStore.newUser"
+        width="400px"
+        :show-close="false"
+        :close-on-click-modal="false"
+        center>
+        <div class="modal_wrap-body">
+          <el-form :model="formNewUser">
+            <el-form-item label="电动车SN" :label-width="formLabelWidth">
+              <el-input v-model="formNewUser.name" auto-complete="off"></el-input>
+            </el-form-item>
+            <el-form-item label="车类型" :label-width="formLabelWidth">
+              <el-select v-model="formNewUser.region" placeholder="请选择活动区域">
+                <el-option label="区域一" value="shanghai"></el-option>
+                <el-option label="区域二" value="beijing"></el-option>
+              </el-select>
+            </el-form-item>
+            <!-- 用户手机号,城市,用户群组,返还金额 -->
+          </el-form>
+        </div>
+        <span slot="footer" class="dialog-footer">
+          <el-button @click="modalStore.groupCreate = false">取 消</el-button>
+          <el-button type="primary" @click="modalStore.groupCreate = false">确 定</el-button>
+        </span>
+      </el-dialog>
+    </div>
+
 
   </div>
 </template>
@@ -96,8 +124,6 @@
 <script>
 import {mapState} from 'vuex';
 import {urls,ajaxs} from '../api/urls.js';
-import BaseTableWrap from '@/components/BaseTableWrap'; //@/=>src/
-import BaseModalWrap from './BaseModalWrap';
 
 export default {
   name:'CUser',
@@ -105,6 +131,11 @@ export default {
     return ({
       total:100,
       searchPhone:'',
+      formNewUser:{
+        name:'',
+        region:''
+      },
+      formLabelWidth:'80px',
       users:[
 
       ],
@@ -125,8 +156,6 @@ export default {
     }
   },
   components:{
-    BaseTableWrap,
-    BaseModalWrap
   },
   methods: {
     fetchData:function(){
