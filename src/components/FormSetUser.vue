74d6312a4547e7556884117315932a9d09d75063
15915940450 <15915940450@139.com>
2018-05-03 10:58:46 +0800

    init form ser user

--- src/components/FormSetUser.vue
+++ src/components/FormSetUser.vue
@@ -3,38 +3,57 @@
     <el-dialog
       title="用户设置"
       :visible.sync="modalStore.setUser"
-      width="390px"
+      width="400px"
       :show-close="false"
       :close-on-click-modal="false"
       center>
       <div class="modal_wrap-body">
-        <el-form :model="formSetUser">
+        <el-form :model="formSetUser" ref="formSetUser">
 
-          <el-form-item label="群组" :label-width="formLabelWidth">
-            <el-input v-model="formSetUser.name" auto-complete="off"></el-input>
+          <el-form-item prop="depositScheme" label="群组" :label-width="formLabelWidth">
+            <el-select v-model="formSetUser.groupCode" placeholder="请选择">
+              <el-option
+                v-for="item in optionsGroups"
+                :key="item.id"
+                :label="(item.name)"
+                :value="item.id">
+              </el-option>
+            </el-select>
           </el-form-item>
 
-          <el-form-item label="押金方案" :label-width="formLabelWidth">
-            <el-select v-model="formSetUser.region" placeholder="请选择活动区域">
-              <el-option label="区域一" value="shanghai"></el-option>
-              <el-option label="区域二" value="beijing"></el-option>
+          <el-form-item prop="depositScheme" label="押金方案" :label-width="formLabelWidth">
+            <el-select v-model="formSetUser.depositScheme" multiple placeholder="请选择">
+              <!-- 199不限次/¥199/月卡套餐/30天/2000次 -->
+              <el-option
+                v-for="item in options_depositListScheme"
+                :key="item.id"
+                :label="(item.name+' / ¥'+item.deposit+' / '+item.num+'颗电池')"
+                :value="item.id">
+              </el-option>
             </el-select>
           </el-form-item>
 
           <el-form-item label="免费天数" :label-width="formLabelWidth">
-            <el-input v-model="formSetUser.name" auto-complete="off" placeholder="剩余可分配天数 100 天"></el-input>
+            <el-input v-model.number="formSetUser.freeDay" auto-complete="off" placeholder="剩余可分配天数 100 天"></el-input>
           </el-form-item>
-          <el-form-item label="可用电池数" :label-width="formLabelWidth">
-            <el-input v-model="formSetUser.name" auto-complete="off" placeholder="剩余可分配天数 100 天"></el-input>
-          </el-form-item>
-          <el-form-item label="电动车" :label-width="formLabelWidth">
-            <el-input v-model="formSetUser.name" auto-complete="off" placeholder="剩余可分配天数 100 天"></el-input>
+          <!-- <el-form-item label="可用电池数" :label-width="formLabelWidth">
+            <el-input v-model.number="formSetUser.name" auto-complete="off" placeholder="剩余可分配电池 100 天"></el-input>
+          </el-form-item> -->
+          <el-form-item prop="scooterSN" label="车牌号(SN)" :label-width="formLabelWidth">
+            <el-select v-model="formSetUser.scooterSN" filterable placeholder="请选择">
+              <el-option
+                v-for="item in optionsEVs"
+                :key="item.sn"
+                :label="item.sn"
+                :value="item.sn">
+              </el-option>
+            </el-select>
           </el-form-item>
 
         </el-form>
       </div>
       <span slot="footer" class="dialog-footer">
-        <el-button @click="modalStore.setUser = false">取 消</el-button>
+        <el-button @click="handleCancel('formSetUser')">取 消</el-button>
         <el-button type="primary" @click="modalStore.setUser = false">确 定</el-button>
       </span>
     </el-dialog>
@@ -43,6 +62,7 @@
 
 <script>
 import {mapState} from 'vuex';
+import {urls,ajaxs} from '../api/urls.js';
 
 export default {
   name:'FormSetUser',
@@ -50,13 +70,82 @@ export default {
     return ({
       formSetUser:{
         name:'',
-        region:''
+        region:'',
+        scooterSN:'',
+        freeDay:'',
+        groupCode:''
       },
+      optionsEVs:[],
+      optionsGroups:[],
+      options_depositListScheme: [],
       formLabelWidth:'100px'
     });
   },
   computed:{
     ...mapState(['modalStore'])
+  },
+  methods:{
+    fetchGroupList:function(){
+      var vueThis=this;
+      var advancedParam=JSON.stringify({
+        agentId:window.sessionStorage.agentid
+      });
+      var sendData={
+        advancedParam:advancedParam,
+        pageNum:1,
+        pageSize:969
+      };
+      ajaxs.imGet(urls.groupList,sendData,function(objRps){
+        if(objRps.code===1000){
+          vueThis.optionsGroups=objRps.result.list;
+        }
+      });
+    },
+    fetchOptionsScheme:function(type){
+      var vueThis=this;
+      var advancedParam=JSON.stringify({
+        groupCode:null
+      });
+      var sendData={
+        advancedParam:advancedParam,
+        pageNum:1,
+        pageSize:969
+      };
+      ajaxs.imPostJson(urls[type],sendData,function(objRps){
+        if(objRps.code===1000){
+          if(type==='depositListScheme'){
+            vueThis.options_depositListScheme=objRps.result.list;
+          }
+          if(type==='packageListScheme'){
+            vueThis.options_packageListScheme=objRps.result.list;
+          }
+        }
+      });
+    },
+    handleCancel:function(refName){
+      this.$refs[refName].resetFields();
+      this.$store.commit('hideSetUser');
+    },
+    fetchEVlist:function(){
+      var vueThis=this;
+      var sendData={
+        phone:''+window.sessionStorage.agentphone,
+        pageNum:1,
+        pageSize:96900000
+      };
+      //请求地址
+      ajaxs.imPostForm(urls.evsList,sendData,function(objRps){
+        // console.log(objRps);
+        if(objRps.code===1000){
+          vueThis.optionsEVs=objRps.result.list;
+        }
+      });
+    }
+  },  //methods
+  created:function(){
+    this.fetchOptionsScheme('depositListScheme');
+    this.fetchEVlist();
+    this.fetchGroupList();
   }
 };
 </script>
