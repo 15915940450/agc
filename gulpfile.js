7051b18969c8b62500d38588536597ac4c790627
15915940450 <15915940450@139.com>
2018-05-08 21:05:01 +0800

    backup urls

--- gulpfile.js
+++ gulpfile.js
@@ -4,14 +4,14 @@ var gulpReplace=require('gulp-replace');
 var version='1100';
 
 gulp.task('moveToCMS',function(){
-  //delete
+  //delete(agent/)
   del(['../CMS-FrontEnd/agent/**/*'],{force:true}).then(function(){
 
     //copy dist to /var/www/html/CMS-FrontEnd
     //mind: index.html need to change
     gulp.src(['./dist/**/*','!./dist/index.html'],{base:'./dist/'})
     .pipe(gulp.dest('../CMS-FrontEnd/agent/'));
-    
+
     //copy index.html, replace v=1.9(lodash.ili.js)
     gulp.src('./dist/index.html',{base:'./dist/'})
     .pipe(gulpReplace('?v=1.9','?v=1.9'+version))
@@ -20,6 +20,18 @@ gulp.task('moveToCMS',function(){
   }); //end of del
 });
 
-gulp.task('default',['moveToCMS'],function(){
+gulp.task('backupUrlsJs',function(){
+  //delete(urls.js)
+  del(['../CMS-FrontEnd/thilina_only__do_not_move_or_modify/urls.js'],{force:true}).then(function(){
+
+    //copy dist to /var/www/html/CMS-FrontEnd
+    //mind: index.html need to change
+    gulp.src(['./src/api/urls.js'],{base:'./src/api/'})
+    .pipe(gulp.dest('../CMS-FrontEnd/thilina_only__do_not_move_or_modify/'));
+
+  }); //end of del
+});
+
+gulp.task('default',['moveToCMS','backupUrlsJs'],function(){
   console.log('version:1.9'+version);
 });
