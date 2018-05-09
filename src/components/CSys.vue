bb3002f224b09c44fa3bf1e41341bcb6282103c3
15915940450 <15915940450@139.com>
2018-05-09 13:57:50 +0800

    rules, patten

--- src/components/CSys.vue
+++ src/components/CSys.vue
@@ -2,9 +2,10 @@
   <div class="component_sys">
     <h3 class="title">修改密码</h3>
     <div class="form_wrap">
-      <el-form size="small" label-position="top" label-width="80px" :model="imPW" class="form_pw">
+      <el-form size="small" label-position="top" label-width="80px" :model="imPW" class="form_pw" :rules="rules" ref="formPW">
 
-        <el-form-item label="旧密码">
+        <!-- oldPassword -->
+        <el-form-item label="旧密码" prop="oldPassword">
           <el-input :type="typeOld" placeholder="请输入您的旧密码" v-model="imPW.oldPassword">
             <i
               class="el-icon-view el-input__icon"
@@ -13,8 +14,8 @@
             </i>
           </el-input>
         </el-form-item>
-
-        <el-form-item label="新密码">
+        <!-- newPassword -->
+        <el-form-item label="新密码" prop="newPassword">
           <el-input :type="typeNew" placeholder="请输入新密码" v-model="imPW.newPassword">
             <i
               class="el-icon-view el-input__icon"
@@ -23,8 +24,8 @@
             </i>
           </el-input>
         </el-form-item>
-
-        <el-form-item label="再输一次新密码">
+        <!-- newPasswordCheck -->
+        <el-form-item label="再输一次新密码" prop="newPasswordCheck">
           <el-input :type="typeCheck" placeholder="请输入新密码" v-model="imPW.newPasswordCheck">
             <i
               class="el-icon-view el-input__icon"
@@ -36,7 +37,7 @@
 
 
         <el-form-item class="btn_wrap">
-            <el-button type="primary" @click="handleEditPW()" :loading="loading">确定</el-button>
+            <el-button type="primary" @click="handleEditPW('formPW')" :loading="loading">确定</el-button>
         </el-form-item>
       </el-form>
     </div>
@@ -61,6 +62,20 @@ export default {
         newPassword: '',
         newPasswordCheck: ''
       },
+      rules:{
+        oldPassword:[
+          {required:true,message:'请输入旧密码',trigger:'blur'},
+          {pattern:/^[0-9a-zA-Z]{6}$/,message:'密码必须是6位数字，字母',trigger:'blur'}
+        ],
+        newPassword:[
+          {required:true,message:'请输入新密码',trigger:'blur'},
+          {pattern:/^[0-9a-zA-Z]{6}$/,message:'密码必须是6位数字，字母',trigger:'blur'}
+        ],
+        newPasswordCheck:[
+          {required:true,message:'请再输一次新密码',trigger:'blur'},
+          {pattern:/^[0-9a-zA-Z]{6}$/,message:'密码必须是6位数字，字母',trigger:'blur'}
+        ]
+      },
       typeOld:'password',
       typeNew:'password',
       typeCheck:'password',
@@ -82,32 +97,37 @@ export default {
     handleHidePW:function(imKey){
       this[imKey]='password';
     },
-    handleEditPW:function(){
+    handleEditPW:function(refName){
       var vueThis=this;
-      vueThis.loading=true;
-      var sendData={
-        phone:window.localStorage.agentphone,
-        oldPassword:vueThis.imPW.oldPassword,
-        newPassword:vueThis.imPW.newPassword
-      };
+      vueThis.$refs[refName].validate((valid) => {
+        if(valid){
+          vueThis.loading=true;
+          var sendData={
+            phone:window.localStorage.agentphone,
+            oldPassword:vueThis.imPW.oldPassword,
+            newPassword:vueThis.imPW.newPassword
+          };
 
-      ajaxs.imPostForm(urls.editPW,sendData,function(objRps){
-        // console.log(objRps);
-        // v-focus
-        if(objRps.code===1000){
-          //修改成功，回到登录页面
-          vueThis.msg='密码修改成功，请重新登录！';
-          vueThis.$store.commit('showStatusEditPW');
-        }else{
-          vueThis.$notify.error({
-            title: '提示',
-            message:objRps.msg,
-            offset: 50,
-            duration: 5000  //0
+          ajaxs.imPostForm(urls.editPW,sendData,function(objRps){
+            // console.log(objRps);
+            // v-focus
+            if(objRps.code===1000){
+              //修改成功，回到登录页面
+              vueThis.msg='密码修改成功，请重新登录！';
+              vueThis.$store.commit('showStatusEditPW');
+            }else{
+              vueThis.$notify.error({
+                title: '提示',
+                message:objRps.msg,
+                offset: 50,
+                duration: 5000  //0
+              });
+            }
+            vueThis.loading=false;
           });
         }
-        vueThis.loading=false;
       });
+      
     }
   }
 };
