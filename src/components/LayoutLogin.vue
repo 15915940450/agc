711cb1698b493fb0ce735a76b7602860583257c2
15915940450 <15915940450@139.com>
2018-05-14 21:42:18 +0800

    localstorage dont set item, grouptype

--- src/components/LayoutLogin.vue
+++ src/components/LayoutLogin.vue
@@ -158,7 +158,6 @@ export default {
       //设置完整登录用户信息
       window.localStorage.setItem('agentname',objRps.result.name);
       window.localStorage.setItem('agentid',objRps.result.id);
-      window.localStorage.setItem('grouptype',objRps.result.groupType);
       this.$store.commit('setAgent',{
         phone:objRps.result.phone,
         name:objRps.result.name,
--- src/store/modules/modalStore.js
+++ src/store/modules/modalStore.js
@@ -29,7 +29,6 @@ var mutations={
     window.localStorage.removeItem('agentid');
     window.localStorage.removeItem('batteryAmount');
     window.sessionStorage.removeItem('payurl');
-    window.localStorage.removeItem('grouptype');
     state.needLogin=true;
     state.baseStatus=false;
     state.topUp=false;
