579bef3d34bf341c53fa14cac4769f6060531c5d
15915940450 <15915940450@139.com>
2018-05-15 14:11:23 +0800

    zyut mong dik siu rung

--- src/components/AjaxTest.vue
+++ src/components/AjaxTest.vue
@@ -1,55 +1,58 @@
 <template lang="html">
   <div class="ajax_test">
-    <ul>
-      <li><a href="">kkk</a></li>
-      <li><a href="">ooo</a></li>
-      <li><a href=""></a></li>
-      <li><a href=""></a></li>
-      <li><a href=""></a></li>
-      <li><a href=""></a></li>
-      <li><a href=""></a></li>
-      <li><a href=""></a></li>
-      <li><a href=""></a></li>
-    </ul>
+    zyut mong dik siu jung
   </div>
 </template>
-
 <script>
-import Vue from 'vue';
-import rqs from '../api/index.js';
-Vue.prototype.$rqs=rqs;
+import {mapState} from 'vuex';
 
-
-
-// //comments and componentnames(router s) and notify | 1000
-
-
-// arr=['abc','def','g','hi','jklmn']
 export default {
   name:'AjaxTest',
-  data() {
-    return {
-      thilina:'kkk'
-    };
+  data:function(){
+    return ({
+      total:0,
+      search:'',
+      isNotSearch:true,
+      THILINA:[
+      ],
+      msg:'',
+      pageNum:(window.Number(this.$route.params.pn)?window.Number(this.$route.params.pn):1)
+    });
   },
-  methods:{
-    load:function(){
-
-      this.$rqs('http://119.23.133.72:8764/agent/account/baseInfo','POSTform',function(rps){console.log(rps);},{
-        objSendData:{
-          phone:15915900001
-        },
-        isLoginRqs:true
+  computed:{
+    ...mapState(['agent','modalStore'])
+  },
+  watch:{
+    pageNum:function(){
+      this.fetchData();
+    },
+    'modalStore.needLogin':function(val){
+      if(!val){
+        this.fetchData();
+      }
+    }
+  },
+  components:{
+  },
+  methods: {
+    fetchData:function(){
+      var vueThis=this;
+      vueThis.$rqs(vueThis.$yApi.THILINA,function(objRps){
       });
+    },
+    imSearch:_.debounce(function(){
+      this.isNotSearch=false;
+      this.fetchData();
+    },690),
+    resetSearch:function(){
+      this.search='';
+      this.fetchData();
     }
   },
   created:function(){
-    this.load();
+    this.fetchData();
+    _.logErr(JSON.stringify(this.$store.state.modalStore));
   }
 };
-
-
 </script>
-
-<style lang="css" scoped>
-</style>
+<style lang="css" scoped></style>
