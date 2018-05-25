2503345c12c56e68bf0b8c52ef09caa08cee2419
15915940450 <15915940450@139.com>
2018-05-25 20:54:51 +0800

    group list, UI

--- src/components/HeartGroup.vue
+++ src/components/HeartGroup.vue
@@ -3,22 +3,56 @@
     <h3 class="title">群组列表</h3>
     <div class="group-list">
       <el-row :gutter="10">
-        <el-col v-for="(item) in group" :span="8" :key="item.id">
+        <el-col v-for="(item) in group" :span="6" :key="item.id">
           <div class="group_card">
-            <h4 class="group_card-title">{{item.name}}</h4>
-            <p>{{item.count}} 个用户</p>
-            <p>{{item.deposits.length}} 个押金方案</p>
-            <p>{{item.packages.length}} 种套餐方案</p>
-            <p><strong v-if="item.type===2">不</strong> 可退押金(线上下交押金)</p>
+            <div class="group_card-header">
+              <el-row>
+                <el-col :span="8">
+                  <div class="group_online"></div>
+                </el-col>
+                <el-col :span="16">
+                  <h4 class="group_card-title overtext">{{item.name}}</h4>
+                  <p class="group_type">
+                    线<span v-if="item.type===1">上</span><span v-if="item.type===2">下</span>交押金
+                  </p>
+                </el-col>
+              </el-row>
+            </div>
+
+            <el-row>
+              <el-col :span="12">
+                <p>用户数量(个）</p>
+              </el-col>
+              <el-col :span="12">
+                <p class="p_val">{{item.count}}</p>
+              </el-col>
+            </el-row>
+            <el-row>
+              <el-col :span="12">
+                <p>押金方案(个）</p>
+              </el-col>
+              <el-col :span="12">
+                <p class="p_val">{{item.deposits.length}}</p>
+              </el-col>
+            </el-row>
+            <el-row>
+              <el-col :span="12">
+                <p>套餐方案(种）</p>
+              </el-col>
+              <el-col :span="12">
+                <p class="p_val">{{item.packages.length}}</p>
+              </el-col>
+            </el-row>
+
             <div class="group_card-footer">
-              <el-button type="primary" size="mini" @click="groupSet(item)">设置</el-button>
-              <el-button type="primary" size="mini" @click="rrPush(item)">
+              <el-button type="default" @click="groupSet(item)">设置</el-button>
+              <el-button type="primary" @click="rrPush(item)">
                 查看用户
               </el-button>
             </div>
           </div>
         </el-col>
-        <el-col :span="8">
+        <el-col :span="6">
           <a href="javascript:;" @click="groupCreate()">
             <div class="group_card group_card-new">
               <p class="group_card-title"><i class="el-icon-plus"></i> 添加群组</p>
@@ -118,32 +152,56 @@ export default {
     background: #FFF;
   }
   .group-list{
-    padding:10px 20px;
+    padding:15px;
   }
   .group_card{
-    padding: 20px;
-    box-shadow:2px 0px 6px rgba(204,204,204,0.21);
-    height:180px;
-    max-height: 180px;
-    border: 1px solid black; 
+    padding:15px;
+    box-shadow:2px 0px 6px rgba(0,0,0,0.1);
+    box-sizing: border-box;
+    height: 250px;
+    margin-bottom: 15px;
+  }
+
+  .group_card-header{
+    margin-bottom: 25px;
+  }
+  .group_online{
+    width: 46px;
+    height: 46px;
+    background: #fe7e73 url(../assets/online.png) no-repeat center center;
+    border-radius: 96px;
   }
   .group_card-title{
     color: #333;
+    font-size: 14px;
+    font-weight: 500;
+    text-align: right;
+  }
+  .group_type{
+    text-align: right;
+    font-weight: 300;
+    color:#555;
+  }
+  .group_card p{
     font-size: 12px;
+    margin: 5px 0;
+  }
+  .p_val{
+    color: #FF6666;
+    font-weight: 600;
+    text-align: right;
   }
   .group_card-new .group_card-title{
     display: block;
-    height: 180px;
-    line-height: 180px;
+    height: 220px;
+    line-height: 220px;
     text-align: center;
     font-size: 12px;
     color: #333;
     font-weight: bold;
+    margin-left:-10px;
   }
-  .group_card p{
-    font-size: 10px;
-    margin: 5px 0;
-  }
+  
   .group_card-footer{
     text-align:center;
     margin-top: 20px;
