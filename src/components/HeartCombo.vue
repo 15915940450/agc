70ee987d1ba7731014043144f9ef3e313bd7e05d
15915940450 <15915940450@139.com>
2018-06-11 13:58:39 +0800

    title with sub

--- src/components/HeartCombo.vue
+++ src/components/HeartCombo.vue
@@ -1,6 +1,13 @@
 <template lang="html">
   <div class="component_combo eqcalc">
-    <h3 class="title">套餐管理</h3>
+    <h3 class="title with_sub">
+      套餐管理
+      <p>
+        在此页面查看拥有的套餐，分配套餐给用户时会消耗套餐的数量，数量可购买。
+        <a href="javascript:;">购买记录</a>
+      </p>
+    </h3>
+    
   </div>
 </template>
 
@@ -21,5 +28,25 @@ export default {
 </script>
 
 <style lang="css" scoped>
-  
+  .component_combo{
+    background: white;
+  }
+  h3.title.with_sub{
+    height: 80px;
+    position: relative;
+  }
+  h3.title.with_sub p{
+    position: absolute;
+    left: 0;
+    bottom: 0;
+    padding:0 15px;
+    margin: 0;
+    font-size: 13px;
+    width: 100%;
+    box-sizing: border-box;
+  }
+  h3.title.with_sub a{
+    float: right;
+    font-weight: 500;
+  }
 </style>
