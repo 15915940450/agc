c4a6ed11036abd7c3d12f88f77a03d8ba594934d
15915940450 <15915940450@139.com>
2018-06-20 18:06:09 +0800

    ref,bind rules,prop,rFs

--- src/components/FormComboBuy.vue
+++ src/components/FormComboBuy.vue
@@ -8,14 +8,20 @@
       :close-on-click-modal="false"
       center>
       <div class="modal_wrap-body">
-        <el-form :model="formComboBuy">
+        <el-form :model="formComboBuy" ref="vuefoem" :rules="rules">
           
           <el-form-item label="套餐名称" :label-width="formLabelWidth">
             <strong>{{discount_name}}</strong>
           </el-form-item>
 
-          <el-form-item label="数量(个)" :label-width="formLabelWidth">
-            <el-input-number v-model="formComboBuy.number" :min="1" :max="9999" @input.native="handleIN"></el-input-number>
+          <el-form-item label="数量(个)" :label-width="formLabelWidth" prop="number">
+            <el-input-number 
+              v-model="formComboBuy.number" 
+              :min="1" 
+              :max="9999" 
+              @input.native="handleIN"
+              >
+            </el-input-number>
           </el-form-item>
 
           <el-form-item label="金额(元)" :label-width="formLabelWidth">
@@ -34,7 +40,7 @@
       </div>
       <span slot="footer" class="dialog-footer">
         <el-button @click="handleCancel()">取 消</el-button>
-        <a :href="payurl" target="_blank" @click="handleComfirm()">
+        <a :href="payurl" :target="target" @click="handleComfirm()">
           <el-button type="primary">确 定</el-button>
         </a>
       </span>
@@ -51,6 +57,12 @@ export default {
   props:['agentPrice','discountName','code'],
   data:function(){
     return ({
+      rules:{
+        number:[
+          {required:true,message:'请输入',trigger:'blur'},
+          {type:'integer',message:'请输入一个整数',trigger:'blur'}
+        ]
+      },
       title:'',
       discount_name:'',
       formComboBuy:{
@@ -72,33 +84,43 @@ export default {
       return (window.Number(this.agentPrice) * window.Number(num));
     },
     payurl:function(){
-      var payurl='';
-      var objSendData={
-        phone:''+window.localStorage.agentphone,
-        discountCode:this.discountCode,
-        payType:window.Number(this.formComboBuy.payType),  //支付类型 1支付宝，2微信
-        number:window.Number(this.formComboBuy.number),
-        amount:(window.Number(this.amount)).toFixed(2),
-        discountName:this.discount_name,
-        from:'comboBuy'
-      };
-      //stringify, then encodeURIComponent
-      var yap=window.encodeURIComponent(JSON.stringify(objSendData));
+      var payurl='javascript:;';
+      
+      if(window.Number.isInteger(window.Number(this.formComboBuy.number))){
+        var objSendData={
+          phone:''+window.localStorage.agentphone,
+          discountCode:this.discountCode,
+          payType:window.Number(this.formComboBuy.payType),  //支付类型 1支付宝，2微信
+          number:window.Number(this.formComboBuy.number),
+          amount:(window.Number(this.amount)).toFixed(2),
+          discountName:this.discount_name,
+          from:'comboBuy'
+        };
+        //stringify, then encodeURIComponent
+        var yap=window.encodeURIComponent(JSON.stringify(objSendData));
 
-      if(this.formComboBuy.payType==='1'){
-        payurl=window.encodeURI('/pay_ali.html?yap='+yap);
-      }else{
-        payurl=window.encodeURI('/pay_wx.html?yap='+yap);
-      }
-      /*==payurl
-      /pay_ali.html?yap=%257B%2522phone%2522%253A%252215915900000%2522%252C%2522type%2522%253A1%252C%2522payType%2522%253A1%252C%2522amount%2522%253A0.5%252C%2522batteryNum%2522%253A1%252C%2522status%2522%253A1%257D
-      */
-      if(window.location.hostname==='localhost'){
-        // http://localhost/agc/dist/pay_ali.html
-        payurl='http://localhost/agc/dist'+payurl;
+        if(this.formComboBuy.payType==='1'){
+          payurl=window.encodeURI('/pay_ali.html?yap='+yap);
+        }else{
+          payurl=window.encodeURI('/pay_wx.html?yap='+yap);
+        }
+        /*==payurl
+        /pay_ali.html?yap=%257B%2522phone%2522%253A%252215915900000%2522%252C%2522type%2522%253A1%252C%2522payType%2522%253A1%252C%2522amount%2522%253A0.5%252C%2522batteryNum%2522%253A1%252C%2522status%2522%253A1%257D
+        */
+        if(window.location.hostname==='localhost'){
+          // http://localhost/agc/dist/pay_ali.html
+          payurl='http://localhost/agc/dist'+payurl;
+        }
       }
 
       return (payurl);
+    },
+    target:function(){
+      var target='';
+      if(window.Number.isInteger(window.Number(this.formComboBuy.number))){
+        target='_blank';
+      }
+      return target;
     }
   },
   watch:{
@@ -121,22 +143,29 @@ export default {
     }
   },
   methods:{
-    handleCancel:function(){
+    rFs:function(){
       var vueThis=this;
       vueThis.formComboBuy.number=1;
       vueThis.formComboBuy.payType='1';
       vueThis.$store.commit('hideComboBuy');
+    },
+    handleCancel:function(){
+      this.rFs();
     },
     handleIN:function(ev){
       this.formComboBuy.number=ev.target.value;
     },
     handleComfirm:function(){
       var vueThis=this;
-      //跳往支付页面(监听充值状态)，显示是否成功
-      vueThis.$store.commit('hideComboBuy');
-      vueThis.$store.commit('showStatusTopUp');
-      //set item payurl
-      window.sessionStorage.setItem('payurl',vueThis.payurl);
+      vueThis.$refs.vueform.validate((valid) => {
+        if(valid){
+          vueThis.rFs();
+          //跳往支付页面(监听充值状态)，显示是否成功
+          vueThis.$store.commit('showStatusTopUp');
+          //set item payurl
+          window.sessionStorage.setItem('payurl',vueThis.payurl);
+        }
+      });
     },
     listenTradeCheck:function(){
       var vueThis=this;
