35df2253d5bb4eb75af51aa06cf8c0ee0fa29252
15915940450 <15915940450@139.com>
2018-07-28 14:12:19 +0800

    im snow

---
+++ src/components/HeartUserChangePhone.vue
@@ -0,0 +1,329 @@
+<template lang="html">
+  <div>
+    <!-- 面包泶 -->
+    <div class="im_breadcrumb">
+      <el-breadcrumb separator="/">
+        <el-breadcrumb-item :to="{ path: '/group' }">
+          <small>群组管理</small>
+        </el-breadcrumb-item>
+        <el-breadcrumb-item>
+          <small>修改手机</small>
+        </el-breadcrumb-item>
+      </el-breadcrumb>
+    </div>
+
+    <div class="im_snow">
+      <div class="im_snow_title">
+        <el-row>
+          <el-col :span="6">
+            <h3 class="title">修改手机号</h3>
+          </el-col>
+          <el-col :span="18">
+            <div class="title_link">
+              <a href="javascript:;">修改记录</a>
+            </div>
+          </el-col>
+        </el-row>
+      </div>
+      <div class="im_form">
+        <el-form :inline="true">
+          <el-form-item label="旧手机号码:">
+            <el-input placeholder="请输入手机号码"></el-input>
+          </el-form-item>
+          <el-form-item label="新手机号码:">
+            <el-input placeholder="请输入手机号码"></el-input>
+          </el-form-item>
+          <el-form-item>
+            <el-button type="primary">查询</el-button>
+          </el-form-item>
+        </el-form>
+      </div>
+      <div class="im_info">
+        <el-card class="box-card">
+          <div slot="header">
+            <table width="100%">
+              <thead>
+                <tr>
+                  <td width="15"></td>
+                  <td width="35"></td>
+                  <td width="15"></td>
+                  <td width="35"></td>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>ID</td>
+                  <td>2098120123</td>
+                  <td>ID</td>
+                  <td>2098140222</td>
+                </tr>
+
+                <tr>
+                  <td>押金</td>
+                  <td>【租两颗电池/1788元/2颗电池】</td>
+                  <td>押金</td>
+                  <td>─</td>
+                </tr>
+                <tr>
+                  <td>套餐</td>
+                  <td>
+                    <ul>
+                      <li>限量套餐月卡/月套餐/199元/90次/30天】</li>
+                      <li>限量套餐月卡/月套餐/199元/90次/30天】</li>
+                      <li>限量套餐月卡/月套餐/199元/90次/30天】</li>
+                    </ul>
+                  </td>
+                  <td>套餐</td>
+                  <td>
+                    <ul>
+                      <li>限量套餐月卡/月套餐/199元/90次/30天】</li>
+                      <li>限量套餐月卡/月套餐/199元/90次/30天】</li>
+                    </ul>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+          <div>
+            <el-button type="danger">修改</el-button>
+          </div>
+        </el-card>
+      </div>
+    </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name:'HeartUserChangePhone',
+  data:function(){
+    return ({
+      twoUsers:[
+        {
+          'id': 5970,   //id
+          'amount': 0,   //钱包余额
+          'phone': '12321211111', // 电话
+          'groupCode': 11, // 群组code
+          'groupName': '测试群组1', // 群组name
+          'freeDays': '', // 免费天数
+          'agentId': 2, // 代理商id
+          'agentName': 'chao', // 代理商name
+          'depositDO': { //押金
+            'id': 2582,
+            'amount': 0.01, //价格
+            'createTime': 1530674017000,
+            'num': 2, //电池颗数
+            'outTradeNo': 'ehd201807041112370000001284',
+            'status': 1,
+            'type': 1,  // 押金类型 2线下 1线上
+            'userId': 4636,
+            'discountId': 74,
+            'outRefundNo': '',
+            'groupCode': '',
+            'depositCode': 49,
+            'depositName': 'test-2颗电池' //押金名字
+          },
+          'validPackage': [ //有效套餐
+            {
+              'id': 1489,
+              'name': '2个月免费骑',  //套餐名字
+              'createTime': 1531369175000,
+              'duration': 60,  //套餐时长
+              'price': 0, //价格
+              'status': 1,
+              'times': 20000, //骑行次数
+              'type': 2, // 套餐类型  0=月套餐 1=次套餐 2=免费套餐
+              'originalPrice': 0,
+              'code': 0,
+              'groupCode': 2,
+              'freeCode': 1,
+              'remark': '',
+              'agentPrice': ''
+            },
+            {
+              'id': 354,
+              'name': '换电套餐2',
+              'createTime': 1527305062000,
+              'duration': 30,
+              'price': 58.01,
+              'status': 1,
+              'times': 30,
+              'type': 0,
+              'originalPrice': 58.01,
+              'code': 64,
+              'groupCode': 2,
+              'freeCode': 0,
+              'remark': '',
+              'agentPrice': ''
+            }
+          ],
+          'scootersList': [  //中控
+            {
+              'id': '61e1c747e7264fb392b966f80b8ff68b',
+              'sid': '574C54DF6591',
+              'owner': '13682680317',
+              'sn': 'G6000000001', // sn
+              'status': 1,
+              'time': 1529027725000,
+              'updtime': 1532656780000,
+              'protime': 1528992000000,
+              'userName': '',
+              'soc': '',
+              'location': '',
+              'locationUpdateTime': '',
+              'agentId': 0,
+              'agentName': '',
+              'voltage': 0,
+              'agentPhone': '',
+              'country': 'US',
+              'conf': 'US',
+              'fw': '',
+              'hw': ''
+            }
+          ],
+          'batteriesList': [ //电池
+            {
+              'id': 232,
+              'bid': '100bbc07838a',
+              'bindTime': 1530607147000,
+              'sn': '2B1317BJ01210', //sn
+              'batVersion': 'B1',
+              'capacity': 2200,
+              'cellVersion': 'MF1',
+              'maker': 'WK',
+              'proTime': 1510934400000,
+              'scanTime': 1515805582000,
+              'voltage': 600,
+              'userName': '',
+              'userPhone': '13682680317',
+              'status': '已绑定'
+            }
+          ]
+        },
+        {
+          'id': 5970,   //id
+          'amount': 0,   //钱包余额
+          'phone': '12321211111', // 电话
+          'groupCode': 11, // 群组code
+          'groupName': '测试群组1', // 群组name
+          'freeDays': '', // 免费天数
+          'agentId': 2, // 代理商id
+          'agentName': 'chao', // 代理商name
+          'depositDO': { //押金
+            'id': 2582,
+            'amount': 0.01, //价格
+            'createTime': 1530674017000,
+            'num': 2, //电池颗数
+            'outTradeNo': 'ehd201807041112370000001284',
+            'status': 1,
+            'type': 1,  // 押金类型 2线下 1线上
+            'userId': 4636,
+            'discountId': 74,
+            'outRefundNo': '',
+            'groupCode': '',
+            'depositCode': 49,
+            'depositName': 'test-2颗电池' //押金名字
+          },
+          'validPackage': [ //有效套餐
+            {
+              'id': 1489,
+              'name': '2个月免费骑',  //套餐名字
+              'createTime': 1531369175000,
+              'duration': 60,  //套餐时长
+              'price': 0, //价格
+              'status': 1,
+              'times': 20000, //骑行次数
+              'type': 2, // 套餐类型  0=月套餐 1=次套餐 2=免费套餐
+              'originalPrice': 0,
+              'code': 0,
+              'groupCode': 2,
+              'freeCode': 1,
+              'remark': '',
+              'agentPrice': ''
+            },
+            {
+              'id': 354,
+              'name': '换电套餐2',
+              'createTime': 1527305062000,
+              'duration': 30,
+              'price': 58.01,
+              'status': 1,
+              'times': 30,
+              'type': 0,
+              'originalPrice': 58.01,
+              'code': 64,
+              'groupCode': 2,
+              'freeCode': 0,
+              'remark': '',
+              'agentPrice': ''
+            }
+          ],
+          'scootersList': [  //中控
+            {
+              'id': '61e1c747e7264fb392b966f80b8ff68b',
+              'sid': '574C54DF6591',
+              'owner': '13682680317',
+              'sn': 'G6000000001', // sn
+              'status': 1,
+              'time': 1529027725000,
+              'updtime': 1532656780000,
+              'protime': 1528992000000,
+              'userName': '',
+              'soc': '',
+              'location': '',
+              'locationUpdateTime': '',
+              'agentId': 0,
+              'agentName': '',
+              'voltage': 0,
+              'agentPhone': '',
+              'country': 'US',
+              'conf': 'US',
+              'fw': '',
+              'hw': ''
+            }
+          ],
+          'batteriesList': [ //电池
+            {
+              'id': 232,
+              'bid': '100bbc07838a',
+              'bindTime': 1530607147000,
+              'sn': '2B1317BJ01210', //sn
+              'batVersion': 'B1',
+              'capacity': 2200,
+              'cellVersion': 'MF1',
+              'maker': 'WK',
+              'proTime': 1510934400000,
+              'scanTime': 1515805582000,
+              'voltage': 600,
+              'userName': '',
+              'userPhone': '13682680317',
+              'status': '已绑定'
+            }
+          ]
+        }
+      ]
+    });
+  },
+  created:function(){}
+};
+</script>
+
+<style lang="css" scoped>
+.im_snow_title{
+  border-bottom:1px solid #F8F8FF;
+}
+.title_link{
+  height:50px;
+  line-height:50px;
+  text-align:right;
+  padding-right:15px;
+}
+.im_form{
+  padding:50px 15px 20px;
+}
+.im_info{
+  padding:0 15px;
+  margin-bottom:20px;
+}
+</style>
