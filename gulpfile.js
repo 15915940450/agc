102ab4045bb1c7affc4e6c6858eab59cdf8b729d
15915940450 <15915940450@139.com>
2018-08-01 15:44:10 +0800

    15.0.0

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
 var gulp=require('gulp');
 var del=require('del');
 var gulpReplace=require('gulp-replace');
-var version='V14.0.4';
+var version='V15.0.0';
 
 //1.backupDist
 gulp.task('backupDist',function(){
