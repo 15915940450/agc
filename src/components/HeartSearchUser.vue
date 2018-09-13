87f0b1bb23a442654ab7d482d7eb22a4130d44cf
15915940450 <15915940450@139.com>
2018-09-13 16:48:52 +0800

    userphone in localstorage

--- src/components/HeartSearchUser.vue
+++ src/components/HeartSearchUser.vue
@@ -5,14 +5,23 @@
       <img src="../assets/search_user.png" alt="search_user" />
 
       <!-- el-autocomplete -->
-      <el-input
+      <el-autocomplete
+        class="search_input"
+        v-model="userPhone"
+        :fetch-suggestions="querySearch"
+        placeholder="请输入内容"
+        suffix-icon="el-icon-search"
+        @select="handleSelect"
+        >
+      </el-autocomplete>
+      <!-- <el-input
         class="search_input"
         placeholder="输入手机号码查询用户"
         suffix-icon="el-icon-search"
         autofocus="true"
         auto-complete="on"
         >
-      </el-input>
+      </el-input> -->
       <p class="latest_add">
         最近添加： 
         <a href="javascript:;">13800138000</a>
@@ -28,6 +37,8 @@ export default {
   name:'HeartSearchUser',
   data:function(){
     return ({
+      userPhone: '',
+      userphoneINlocalstorage: []
     });
   },
   computed:{
@@ -44,8 +55,36 @@ export default {
   components:{
   },
   methods: {
+    querySearch(queryString, cb) {
+      var userphoneINlocalstorage = this.userphoneINlocalstorage;
+      var results = queryString ? userphoneINlocalstorage.filter(this.createFilter(queryString)) : userphoneINlocalstorage;
+      // 调用 callback 返回建议列表的数据
+      cb(results);
+    },
+    createFilter(queryString) {
+      return (restaurant) => {
+        return (restaurant.value.toLowerCase().indexOf(queryString.toLowerCase()) === 0);
+      };
+    },
+    loadAll() {
+      return [
+        { "value": "三全鲜食（北新泾店）", "address": "长宁区新渔路144号" },
+        { "value": "Hot honey 首尔炸鸡（仙霞路）", "address": "上海市长宁区淞虹路661号" },
+        { "value": "新旺角茶餐厅", "address": "上海市普陀区真北路988号创邑金沙谷6号楼113" },
+        { "value": "泷千家(天山西路店)", "address": "天山西路438号" },
+        { "value": "胖仙女纸杯蛋糕（上海凌空店）", "address": "上海市长宁区金钟路968号1幢18号楼一层商铺18-101" },
+        { "value": "贡茶", "address": "上海市长宁区金钟路633号" }
+      ];
+    },
+    handleSelect(item) {
+      _.logErr(item);
+    }
   },
   created:function(){
+    
+  },
+  mounted:function(){
+    this.userphoneINlocalstorage = this.loadAll();
   }
 };
 </script>
@@ -64,6 +103,7 @@ export default {
   margin-top:calc(50vh - 71px - 50px - 144px);
 }
 .search_input{
+  width: 330px;
   margin-top: 20px;
   margin-bottom: 20px;
 }
