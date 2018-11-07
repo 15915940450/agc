ff4384a81e34d22f639d5755c31acf1435ce0385
15915940450 <15915940450@139.com>
2018-11-07 00:46:42 -0500

    replace 2 php

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@
 var gulp=require('gulp');
 var del=require('del');
 var gulpReplace=require('gulp-replace');
-var version='V18.0.1';
+var version='V18.0.4';
 
 //1.backupDist
 gulp.task('backupDist',function(){
@@ -12,7 +12,11 @@ gulp.task('backupDist',function(){
     //mind: index.html need to change
     gulp.src(['./dist/**/*','!./dist/index.html'],{base:'./dist/'}).pipe(gulp.dest('../CMS-FrontEnd/agent/'));
     //copy index.html, replace v=1.5.9(lodash.ili.js)
-    gulp.src('./dist/index.html',{base:'./dist/'}).pipe(gulpReplace('<!DOCTYPE html><html>','<!DOCTYPE html><html v="'+version+'">')).pipe(gulpReplace('lodash.ili.js?v=1.5.9','lodash.ili.js?v='+version)).pipe(gulp.dest('../CMS-FrontEnd/agent/'));
+    gulp.src('./dist/index.html',{base:'./dist/'})
+    .pipe(gulpReplace('<!DOCTYPE html><html>','<!DOCTYPE html><html v="'+version+'">'))
+    .pipe(gulpReplace('lodash.ili.js?v=1.5.9','lodash.ili.js?v='+version))
+    .pipe(gulpReplace('<script id="php"></script>','<script type="text/javascript">var serverTime=window.Number(""+<?php echo time(); ?>+"000");var dobjServerTime=new Date(serverTime);var offsetMS=+(-480-(dobjServerTime.getTimezoneOffset()))*60*1000;console.log("==dobjServerTime==" 服務器時間：("+serverTime+")    "+dobjServerTime); </script>'))
+    .pipe(gulp.dest('../CMS-FrontEnd/agent/'));
   }); //end of del
 });
 
